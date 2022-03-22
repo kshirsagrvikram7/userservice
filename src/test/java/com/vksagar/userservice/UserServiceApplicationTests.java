84fc17dd6717e38fbd013d6aac6d13d879926292
unknown <Vikram Kshirsagar@DESKTOP-24PCHH7>
2022-03-22 22:07:44 +0530

    Userservice added

---
+++ src/test/java/com/vksagar/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vksagar.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
